5c0b9d23764cf6c9f18d0645fb3c8462f9918142
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-16 23:40:55 -0700

    Render HTML

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -10,7 +11,14 @@ func main() {
 	fmt.Println("Server is starting...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		t, err := template.ParseFiles("templates/index.html")
+
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err)
+			return
+		}
+
+		t.Execute(w, struct{}{})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
