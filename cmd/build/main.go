8ecdefa0eb283dd0aecd5fb63740cbeac6077088
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-23 22:23:30 -0700

    Debug

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/wham/minimalist-go-react-app/v2/internal/ui"
 )
 
 func main() {
+	slog.Info("Building UI for production...")
 	data := ui.BuildForProduction()
 
 	os.WriteFile("build/ui.js", data, 0644)
+	slog.Info("Wrote file: build/ui.js")
 }
