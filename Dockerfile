95302f0dc0e7d37d0a2e3c6da9d7332b9b8ab767
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-25 14:28:25 -0700

    clean dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,12 @@ FROM alpine:latest AS builder
 RUN apk add --update nodejs npm
 COPY --from=golang:1.22.4-alpine /usr/local/go/ /usr/local/go/
 ENV PATH="/usr/local/go/bin:${PATH}"
-ARG APP_NAME
-COPY . /workspace/$APP_NAME
-WORKDIR /workspace/$APP_NAME
+WORKDIR /workspace
+COPY . .
 RUN npm ci
-RUN ./scripts/test
-RUN mkdir -p ./build
 RUN ./scripts/build
 
-# FROM alpine:latest AS runner
-# COPY --from=builder /workspace/build/minimalist-go-react-app /app/
-# WORKDIR /app
-# EXPOSE 8080
-# CMD ["./minimalist-go-react-app"]
+FROM alpine:latest AS runner
+COPY --from=builder /workspace/build/minimalist-go-react-app /
+EXPOSE 8080
+CMD ["/minimalist-go-react-app"]
--- cmd/build/main.go
+++ cmd/build/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"os"
+	"path/filepath"
 
 	"github.com/wham/minimalist-go-react-app/v2/internal/ui"
 )
@@ -11,6 +12,16 @@ func main() {
 	slog.Info("Building UI for production...")
 	data := ui.BuildForProduction()
 
-	os.WriteFile("build/ui.js", data, 0644)
-	slog.Info("Wrote file: build/ui.js")
+	outputDir := "build"
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		slog.Error("Failed to create directory", "error", err)
+		return
+	}
+
+	outputFile := filepath.Join(outputDir, "ui.js")
+	if err := os.WriteFile(outputFile, data, 0644); err != nil {
+		slog.Error("Failed to write file", "error", err)
+		return
+	}
+	slog.Info("Wrote file:", "file", outputFile)
 }
