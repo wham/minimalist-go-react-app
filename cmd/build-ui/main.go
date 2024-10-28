df33a58dfac7d4ce06e248cecdea22b239dd6409
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-27 18:36:16 -0700

    Refactored main.go

--- cmd/build-ui/main.go
+++ cmd/build-ui/main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"log/slog"
 	"os"
-	"path/filepath"
+	"path"
 
 	"github.com/wham/minimalist-go-react-app/v2/internal/ui"
 )
 
 func main() {
+	if err := build(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func build() error {
 	slog.Info("Building UI for production...")
+
 	data := ui.BuildForProduction()
 
-	outputDir := "build"
-	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-		slog.Error("Failed to create directory", "error", err)
-		return
+	outputDirectory := "build"
+	if err := os.MkdirAll(outputDirectory, os.ModeDir); err != nil {
+		slog.Error("Failed to create output directory", "error", err)
+		return err
 	}
 
-	outputFile := filepath.Join(outputDir, "ui.js")
-	if err := os.WriteFile(outputFile, data, 0644); err != nil {
-		slog.Error("Failed to write file", "error", err)
-		return
+	outputFile := path.Join(outputDirectory, "ui.js")
+	if err := os.WriteFile(outputFile, data, 0666); err != nil {
+		slog.Error("Failed to write output file", "error", err)
+		return err
 	}
-	slog.Info("Wrote file:", "file", outputFile)
+
+	slog.Info("UI built successfully", "outputFile", outputFile)
+
+	return nil
 }
