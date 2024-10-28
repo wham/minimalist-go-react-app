bb986127cb51656d238d1359ff2575d73d13c712
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-27 20:02:00 -0700

    Refactored main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,54 +2,61 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	assets "github.com/wham/minimalist-go-react-app/v2"
 	"github.com/wham/minimalist-go-react-app/v2/internal/storage"
 )
 
 func main() {
-	fmt.Println("Server is starting...")
+	slog.Info("Starting server...")
 
 	todos := storage.Todos{}
 
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFS(assets.TemplatesFS, "templates/**.html")
+		slog.Info("HTML request", "method", r.Method, "path", r.RequestURI)
 
+		template, err := template.ParseFS(assets.TemplatesFS, "templates/**.html")
 		if err != nil {
-			fmt.Fprintf(w, "Error: %s", err)
+			slog.Error("Failed to parse HTML templates", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
 			return
 		}
 
-		err = t.ExecuteTemplate(w, "index.html", struct{}{})
-
-		if err != nil {
-			fmt.Fprintf(w, "Error: %s", err)
+		if err := template.ExecuteTemplate(w, "index.html", struct{}{}); err != nil {
+			slog.Error("Failed to execute template", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Internal server error"))
 			return
 		}
 	})
 
 	http.HandleFunc("GET /static/{name...}", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Static file request", "method", r.Method, "path", r.RequestURI)
 		http.ServeFileFS(w, r, assets.StaticFS, "static/"+r.PathValue("name"))
 	})
 
 	http.HandleFunc("GET /ui.js", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("UI bundle request", "method", r.Method, "path", r.RequestURI)
+
 		w.Write(assets.ReadUI())
 	})
 
 	http.HandleFunc("GET /api/todos", func(w http.ResponseWriter, r *http.Request) {
-		json, _ := json.Marshal(todos.List())
-		w.Write(json)
+		data, _ := json.Marshal(todos.List())
+		w.Write(data)
 	})
 
 	http.HandleFunc("PUT /api/todos", func(w http.ResponseWriter, r *http.Request) {
-		todo := storage.Todo{}
+		var todo storage.Todo
 		json.NewDecoder(r.Body).Decode(&todo)
 		todos.Add(todo)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	slog.Error("Failed to start server", "error", http.ListenAndServe(":8080", nil))
+	os.Exit(1)
 }
